34bc7e8eef88b8a8cf306815263e5ed018228da6
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-02-07 17:02:47 -0500

    More Electric floor stuff

--- RPGGame/Assets/_Scripts/ElectricFloorManager.cs
+++ RPGGame/Assets/_Scripts/ElectricFloorManager.cs
@@ -9,24 +9,33 @@ public class ElectricFloorManager : MonoBehaviour
     public Color red = Color.red;
     public Color black;
     public Color blue;
+    private SpriteRenderer redIndicator;
 
     void Start()
     {
         wireTiles = GameObject.FindGameObjectsWithTag("Tiles");
-        wireTiles[0].GetComponent<SpriteRenderer>().color = red;
+        redIndicator = GameObject.Find("Red Indicator").GetComponent<SpriteRenderer>();
+        StartCoroutine(setTilesOnline(red));
+        changeTiles(red,wireTiles);
+        setTilesOnline(red);
     }
     
     public void changeTiles(Color colour, params GameObject[] tiles){
         foreach (GameObject tile in tiles){   
-            tile.GetComponent<SpriteRenderer>().color = colour;
+            tile.GetComponent<ElectricTile>().colour = colour;
         }
     }
 
-    public void setTilesOnline(Color colour){
+    public IEnumerator setTilesOnline(Color colour){
+        yield return new WaitForSeconds(2);
         foreach (GameObject tile in wireTiles){
-            if (tile.GetComponent<SpriteRenderer>().color == colour){
+            if (tile.GetComponent<ElectricTile>().colour == colour){
                 tile.GetComponent<BoxCollider2D>().enabled = true;
             }
+            else{
+                tile.GetComponent<BoxCollider2D>().enabled = false;
+            }
         }
+        redIndicator.color += new Color (0, 0, 0, 0.5f);
     }
 }
--- RPGGame/Assets/_Scripts/ElectricTile.cs
+++ RPGGame/Assets/_Scripts/ElectricTile.cs
@@ -7,7 +7,23 @@ public class ElectricTile : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.tag == "Player"){
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
             GameEvents.current.playerGetsDamaged();
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+            if (other.gameObject.tag == "Player"){
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+
+    public Color colour{
+        get {
+            return this.gameObject.GetComponent<SpriteRenderer>().color;
+        }
+        set {
+            this.gameObject.GetComponent<SpriteRenderer>().color = value;
         }
     }
 }
