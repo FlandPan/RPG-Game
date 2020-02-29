27da4df0bd5042a4889b90ddfbc70637d3e9b669
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-02-29 11:58:44 -0500

    Added Scene controller

---
+++ RPGGame/Assets/_Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    private Scene currentSceneIndex;
+
+    void Awake()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene();
+    }
+    public void loadNextLevel(int nextLevelIndex){
+        if (currentSceneIndex != SceneManager.GetSceneByBuildIndex(nextLevelIndex)){
+            SceneManager.LoadScene(nextLevelIndex);
+        }
+    }
+}
