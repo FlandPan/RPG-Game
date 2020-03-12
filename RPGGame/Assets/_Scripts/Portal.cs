47319cc2f9266d39dda03f5a8af50f5e4c226530
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-03-12 17:34:22 -0400

    Added HUB level and portal feature

---
+++ RPGGame/Assets/_Scripts/Portal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Portal : MonoBehaviour
+{
+    public int SceneNumber;
+    public string Location;
+    private UIManager _UI;
+
+    void Awake()
+    {
+        _UI = GameObject.Find("UI").GetComponent<UIManager>();
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject == PlayerSingleton.player){
+            _UI.Display(Location);
+            if (Input.GetKeyDown(KeyCode.E)){
+                SceneManager.LoadScene(SceneNumber);
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == PlayerSingleton.player){
+            _UI.DisplayExit();
+        }
+    }
+}
