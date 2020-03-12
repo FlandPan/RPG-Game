fade27acc8c3d6c0b2dc74f9b30952e82eb93173
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-03-12 17:58:54 -0400

    Added projectile script parent and damage

--- RPGGame/Assets/_Scripts/ProjectileScript.cs
+++ RPGGame/Assets/_Scripts/ProjectileScript.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
+    protected int Damage = 0;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject == PlayerSingleton.player){
-            GameEvents.current.PlayerGetsDamaged(10);
+            GameEvents.current.PlayerGetsDamaged(Damage);
         }
+    }
+    //For each projectile override this method with stats, etc.
+    public void ProjectileDamage(int damage){
+        //Multipliers
+        Damage = damage;
     }
 }
