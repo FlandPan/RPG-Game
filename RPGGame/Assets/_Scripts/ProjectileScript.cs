edd416c0b8699ba84b93bec428a8f5f8af806b34
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2020-03-18 23:13:17 -0400

    Enemies get destroyed but bullets stay

--- RPGGame/Assets/_Scripts/ProjectileScript.cs
+++ RPGGame/Assets/_Scripts/ProjectileScript.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
-    private int Damage = 0;
+    private int Damage = 20;
     void OnTriggerEnter2D(Collider2D other)
     {
         EnemyStats enemy = other.GetComponent<EnemyStats>();
         if (enemy != null)
         {
             enemy.TakeDamage(Damage);
+            Destroy(gameObject);
+        }
+        else{
+            Debug.Log("It's broken fam");
         }
     }
     //For each projectile override this method with stats, etc.
