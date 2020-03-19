a879b2507ddcfd369a5d2b12d8ba792e28f52122
Richard <rlii@uwo.ca>
2020-03-18 20:39:37 -0400

    added enemy spawn to dls level

---
+++ RPGGame/Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    private bool check = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(check == false && Mathf.Floor(Time.time) %5 == 0)
+        {
+            GameObject enemy = Instantiate(enemyPrefab);
+            enemy.transform.position = new Vector3(-10,-5,0);
+            check = true;
+        }
+        else if(check == true &&Mathf.Floor(Time.time) %5 == 1)
+        {
+            check = false;
+        }
+    }
+}
