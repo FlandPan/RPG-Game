0c1d00be2753f1685856b1fb20b87958c1a8db83
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-03-31 15:24:02 -0400

    More coin stuff

--- RPGGame/Assets/_Scripts/TimerManager.cs
+++ RPGGame/Assets/_Scripts/TimerManager.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class TimerManager : MonoBehaviour
 {
     private Text _timerText;
+    private bool _gameRestarted;
     public GameObject canvas;
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
         DontDestroyOnLoad(canvas);
         _timerText = this.gameObject.GetComponent<Text>();
-        Timer.StartCountDown(new System.TimeSpan(0,5,0));
+        GameEvents.current.OnPlayerDeath += GameRestarted;
+        if (!_gameRestarted){
+            Timer.StartCountDown(new System.TimeSpan(0,5,0));
+            _gameRestarted = true;
+        }
+    }
+    public void GameRestarted(){
+        _gameRestarted = false;
     }
     void Update()
     {
