f2b518eca62cc4825464d699368985cb9bff2018
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2020-03-31 04:52:01 -0400

    Added score board basics

---
+++ RPGGame/Assets/_Scripts/ScoreBoard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    private GameObject _player;
+    public GameObject scoreBoardUI;
+    public GameObject mainUI;
+    void Start()
+    {
+        _player = PlayerSingleton.player;
+    }
+    void Update()
+    {
+        if (_player == null)
+        {
+            _player = PlayerSingleton.player;
+        }
+    }
+    public void Exit()
+    {
+        mainUI.SetActive(true);
+        scoreBoardUI.SetActive(false);
+    }
+    public void OpenScoreBoard()
+    {
+        mainUI.SetActive(false);
+        scoreBoardUI.SetActive(true);
+    }
+}
