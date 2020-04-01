462fd73ab57d3f95aa3340a6cc25f6ae02558e42
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2020-04-01 05:39:01 -0400

    Top 3 scores setup and reset button

--- RPGGame/Assets/_Scripts/Score.cs
+++ RPGGame/Assets/_Scripts/Score.cs
@@ -3,12 +3,38 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Text score;
+    public Text score1;
+    public Text score2;
+    public Text score3;
+
+    void Start(){
+        score1.text = "High Score 1: " + PlayerPrefs.GetInt("HS1").ToString();
+        score2.text = "High Score 2: " + PlayerPrefs.GetInt("HS2").ToString();
+        score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3").ToString();
+    }
 
     public void UpdateScore(){
         int number = Random.Range(1,7);
-        score.text = "High Score 1: " + number.ToString();
+        
+        if (number > PlayerPrefs.GetInt("HS1")){
+            PlayerPrefs.SetInt("HS1", number);
+            score1.text = "High Score 1: " + number.ToString();
+        } else if (number > PlayerPrefs.GetInt("HS2")){
+            PlayerPrefs.SetInt("HS2", number);
+            score2.text = "High Score 2: " + number.ToString();
+        } else if (number > PlayerPrefs.GetInt("HS3"))
+        {
+            PlayerPrefs.SetInt("HS3", number);
+            score3.text = "High Score 3: " + number.ToString();
+        }
+    }
 
-        PlayerPrefs.SetInt("HS1", number);
+    public void ResetScores(){
+        PlayerPrefs.DeleteKey("HS1");
+        score1.text = "High Score 1: 0";
+        PlayerPrefs.DeleteKey("HS2");
+        score2.text = "High Score 2: 0";
+        PlayerPrefs.DeleteKey("HS3");
+        score3.text = "High Score 3: 0";
     }
 }
