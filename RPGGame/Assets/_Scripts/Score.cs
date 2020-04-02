b4ba15f4725ef55b50e9db44d1c863eb78bf84a1
Dhyey Bhavsar <dhyeybhavsar2000@gmail.com>
2020-04-02 00:32:06 -0400

    Moved from Update to updateNum

--- RPGGame/Assets/_Scripts/Score.cs
+++ RPGGame/Assets/_Scripts/Score.cs
@@ -6,7 +6,6 @@ public class Score : MonoBehaviour
     public Text score1;
     public Text score2;
     public Text score3;
-    public static int number;
     void Start(){
         score1.text = "High Score 1: " + PlayerPrefs.GetInt("HS1").ToString();
         score2.text = "High Score 2: " + PlayerPrefs.GetInt("HS2").ToString();
@@ -14,27 +13,32 @@ public class Score : MonoBehaviour
     }
 
     void Update(){        
-        if (number > PlayerPrefs.GetInt("HS1")){
-            PlayerPrefs.SetInt("HS3", PlayerPrefs.GetInt("HS2"));
-            PlayerPrefs.SetInt("HS2", PlayerPrefs.GetInt("HS1"));
-            PlayerPrefs.SetInt("HS1", number);
-            score1.text = "High Score 1: " + PlayerPrefs.GetInt("HS1");
-            score2.text = "High Score 2: " + PlayerPrefs.GetInt("HS2");
-            score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3");
-        } else if (number > PlayerPrefs.GetInt("HS2")){
-            PlayerPrefs.SetInt("HS3", PlayerPrefs.GetInt("HS2"));
-            PlayerPrefs.SetInt("HS2", number);
-            score2.text = "High Score 2: " + PlayerPrefs.GetInt("HS2");
-            score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3");
-        } else if (number > PlayerPrefs.GetInt("HS3"))        {
-            PlayerPrefs.SetInt("HS3", number);
-            score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3");
-        }
+        
     }
-    public static void updateNum(int num){
+    public void updateNum(int num){
         if (BossHits.getHP() == 0)
         {
-            number = num;
+            if (num > PlayerPrefs.GetInt("HS1"))
+            {
+                PlayerPrefs.SetInt("HS3", PlayerPrefs.GetInt("HS2"));
+                PlayerPrefs.SetInt("HS2", PlayerPrefs.GetInt("HS1"));
+                PlayerPrefs.SetInt("HS1", num);
+                score1.text = "High Score 1: " + PlayerPrefs.GetInt("HS1");
+                score2.text = "High Score 2: " + PlayerPrefs.GetInt("HS2");
+                score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3");
+            }
+            else if (num > PlayerPrefs.GetInt("HS2"))
+            {
+                PlayerPrefs.SetInt("HS3", PlayerPrefs.GetInt("HS2"));
+                PlayerPrefs.SetInt("HS2", num);
+                score2.text = "High Score 2: " + PlayerPrefs.GetInt("HS2");
+                score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3");
+            }
+            else if (num > PlayerPrefs.GetInt("HS3"))
+            {
+                PlayerPrefs.SetInt("HS3", num);
+                score3.text = "High Score 3: " + PlayerPrefs.GetInt("HS3");
+            }
         }
     }
 
