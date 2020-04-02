5aa011062401e756c180a20adc619605970ce71a
Fland Pan <43575097+FlandPan@users.noreply.github.com>
2020-04-01 23:15:40 -0400

    Boss Scene Win Screen

--- RPGGame/Assets/_Scripts/BossHits.cs
+++ RPGGame/Assets/_Scripts/BossHits.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BossHits : MonoBehaviour
 {
@@ -19,6 +20,9 @@ public class BossHits : MonoBehaviour
         if(other.name !="boomerang(Clone)")
         Destroy(other.gameObject);
         }   
+    }
+    public void Home(){
+        SceneManager.LoadScene(0);
     }
     void Update()
     {
